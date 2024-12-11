b8ebc9ecf60898a877c15dd7aa500e116a8797f1
Your Name <you@example.com>
2024-12-11 23:04:37 +0200

    Answer Q2

--- Assignment2/Program.cs
+++ Assignment2/Program.cs
@@ -9,6 +9,14 @@
             //var Num = Convert.ToDouble(Console.ReadLine());
             //Console.WriteLine(($"Your Number Is {Num}"));
             #endregion
+            #region Write C# program that Assigning one value type variable to another and modifying the value of one variable and mention what will happen
+            //int A = 10;
+            //int B = A;
+            //B = 20;
+            //Console.WriteLine($"First Number: {A}");
+            //Console.WriteLine($"Second Number: {B}");
+            ////Bec Value types stored on the stack ,modifying one does not affect the other
+            #endregion
         }
     }
 }
