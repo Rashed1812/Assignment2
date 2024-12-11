38f018c4898727aaef55e33c5b72f0a59f532119
Your Name <you@example.com>
2024-12-11 23:30:16 +0200

    Answer Q3

--- Assignment2/Program.cs
+++ Assignment2/Program.cs
@@ -1,6 +1,6 @@
 ﻿namespace Assignment2
 {
-    internal class Program
+    internal partial class Program
     {
         static void Main(string[] args)
         {
@@ -17,6 +17,20 @@
             //Console.WriteLine($"Second Number: {B}");
             ////Bec Value types stored on the stack ,modifying one does not affect the other
             #endregion
+            #region Write C# program that Assigning one reference type variable to another and modifying the object through one variable 
+            //Point p1 = new Point();
+            //p1.x = 10;
+            //p1.y = 20;
+            //Point p2 = new Point();
+
+            //p2 = p1;
+            //p2.x = 5;
+            //p2.y = 15;
+            //Console.WriteLine($"p1.x: {p1.x}, p2.y: {p1.y}");
+            //Console.WriteLine($"P2.X: {p2.x}, P2.Y: {p2.y}");
+            ////Bec Reference Type object will effect in all ref points bec share same location memory
+            #endregion
+
         }
     }
 }
